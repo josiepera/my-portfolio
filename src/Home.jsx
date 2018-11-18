c4232e0f6fc8cc6f6f235452e3f1bc44230f05ad
jperalta <jperalta.henri@gmail.com>
2018-11-18 17:53:08 -0500

    draft

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+  render() {
+    return(
+      <div className="home">
+          <Header/>
+        <div className="head">
+
+        </div>
+      </div>
+
+      )
+  }
+}
+
+export default Home;
