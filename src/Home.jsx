e3eea3613045a9789f99386748ec87a056176329
jperalta <jperalta.henri@gmail.com>
2018-11-18 22:32:15 -0500

    added pages and css

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,12 +5,23 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return(
-      <div className="home">
+      <div>
+        <div className="home">
           <Header/>
+          <h1>Josie Peralta</h1>
+          <p>Web Developer</p>
+        </div>
         <div className="head">
-
+          <h1> Second Page</h1>
+        </div>
+        <div className="divider">
+          <h1>Contact Info</h1>
+        </div>
+        <div className="contact">
+          <h1>Contact Info</h1>
         </div>
       </div>
+
 
       )
   }
