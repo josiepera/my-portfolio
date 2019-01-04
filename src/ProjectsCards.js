a6e267a6dafbaedd5ae69171d60de47b62204729
jperalta <jperalta.henri@gmail.com>
2019-01-04 13:25:04 -0500

    changed prject url

--- src/ProjectsCards.js
+++ src/ProjectsCards.js
@@ -44,7 +44,7 @@ class ProjectsCards extends Component {
               <div className="content">
                 <p id="card-title">Who Reps Me</p>
                 <p>Application which users can find who the representatives of their respective states are and their social media profiles.</p>
-                <a href="https://josiepera.github.io/duck_hunt/" target="_blank"><button>Check it out!</button></a>
+                <a href="http://irritating-wind.surge.sh/" target="_blank"><button>Check it out!</button></a>
               </div>
           </div>
 
