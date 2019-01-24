ae7baafbd130a3af7e82af67174685313b342329
jperalta <jperalta.henri@gmail.com>
2019-01-23 22:25:06 -0500

    adjusted the footer and log contact form

--- src/Contact.js
+++ src/Contact.js
@@ -2,31 +2,55 @@ import React, { Component } from 'react';
 import Footer from './Footer'
 
 class Contact extends Component {
+  state= {
+    name: '',
+    email: '',
+    message: '',
+  }
+
+  handleFormSubmit( event ) {
+    event.preventDefault();
+    console.log(this.state);
+}
+
   render(){
 
     return(
+    <>
       <div className="contact-page">
         <h2>Contact Me</h2>
+        <div className="line"></div>
         <div className="contact-form">
           <form>
-            <label>First Name</label>
-            <input type="text"id="fname"name="firstname"placeholder="Your name"/>
-            <label>Last Name</label>
-            <input type="text"id="lname" name="lastname" placeholder="Your last name"/>
+            <label>Name</label>
+            <input type="text"id="name"name="name"placeholder="Your Name"
+            value= {this.state.name}
+            onChange={e => this.setState({ name: e.target.value })}
+            />
+
 
 
             <label>Email</label>
-            <input type="email"id="email"name="email"placeholder="Your email"/>
+            <input type="email"id="email"name="email"placeholder="Your email"
+            value={this.state.email}
+            onChange={e => this.setState({ email: e.target.value })}
+            />
 
 
             <label>Subject</label>
-            <textarea id="subject"name="subject"placeholder="Write something"></textarea>
-            <input type="submit"value="Submit"/>
+            <textarea id="subject"name="subject"placeholder="Write something"
+            onChange={e => this.setState({ message: e.target.value })}
+            value={this.state.message}
+            ></textarea>
+
+            <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
+
           </form>
         </div>
-        <Footer/>
-      </div>
 
+      </div>
+      <Footer/>
+    </>
     )
   }
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 class Footer extends Component{
   render(){
     return(
-      <>
+      <div className="footer">
+
+        <div className="contact">
+          <a href="https://www.linkedin.com/in/josieperalta/" target="_blank"><img className="social-btn" id="linkedin" src="https://i.imgur.com/fgUY2Ts.png" alt="linkedin"/></a>
+          <a href="https://github.com/josiepera" target="_blank"><img className="social-btn" id="github" src="https://i.imgur.com/QCvcF5n.png" alt="github"/></a>
+          <a href="mailto:jperalta.henri@gmail.com" target="_blank"><img className="social-btn" id="email" src="https://i.imgur.com/qsoAXVB.png" alt="email"/></a>
+        </div>
         <h3>© 2018 Josie Peralta</h3>
-      </>
+      </div>
 
     )
   }
